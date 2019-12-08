8af25f69f31412666f786a501beb5c81e181d2a3
Victor <adsvictor@outlook.com>
2019-12-08 20:53:10 -0300

    Create Avatar Image

---
+++ src/components/Avatar/index.js
@@ -0,0 +1,21 @@
+import { useStaticQuery, graphql } from "gatsby"
+import React from "react"
+import Img from "gatsby-image"
+
+const Avatar = () => {
+  const { avatarImage } = useStaticQuery(graphql`
+    query {
+      avatarImage: file(relativePath: { eq: "profile-photo.jpg" }) {
+        childImageSharp {
+          fixed(width: 60, height: 60) {
+            ...GatsbyImageSharpFixed
+          }
+        }
+      }
+    }
+  `)
+
+  return <Img fixed={avatarImage.childImageSharp.fixed} />
+}
+
+export default Avatar

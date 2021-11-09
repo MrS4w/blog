778a2037c8637dbfc6bbf79a2c5290d3df79a1ad
Victor <adsvictor@outlook.com>
2021-11-09 11:10:38 -0300

    :sparkles: feat: add SocialLink component

---
+++ src/components/SocialLinks/content.js
@@ -0,0 +1,24 @@
+const links = [
+  {
+    label: "Github",
+    url: "https://github.com/MrS4w",
+  },
+  {
+    label: "Twitter",
+    url: "https://twitter.com/MrS4w",
+  },
+  {
+    label: "Instagram",
+    url: "https://instagram.com/mrs4ww",
+  },
+  {
+    label: "LinkedinSquare",
+    url: "https://www.linkedin.com/in/victor-antonio",
+  },
+  {
+    label: "Spotify",
+    url: "https://open.spotify.com/user/3n0eju8b0bij2oj5mvzsrqzyr",
+  },
+]
+
+export default links
---
+++ src/components/SocialLinks/icons.js
@@ -0,0 +1,15 @@
+import { Github } from "@styled-icons/boxicons-logos/Github"
+import { Twitter } from "@styled-icons/boxicons-logos/Twitter"
+import { Spotify } from "@styled-icons/boxicons-logos/Spotify"
+import { Instagram } from "@styled-icons/boxicons-logos/Instagram"
+import { LinkedinSquare } from "@styled-icons/boxicons-logos/LinkedinSquare"
+
+const Icons = {
+  Github,
+  Twitter,
+  Spotify,
+  Instagram,
+  LinkedinSquare,
+}
+
+export default Icons
